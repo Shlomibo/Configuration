1897ef264576f7e8fa107bc3bf2149b93427c0f8
Shlomi Borovitz <shlomi.newmail@gmail.com>
2014-01-12 09:43:39 +0200

    Added interface for configuration key

--- Configuration/IConfigKey.cs
+++ Configuration/IConfigKey.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Configuration
 {
-	interface IConfigKey
+	public interface IConfigKey : IEnumerable<KeyValuePair<string, string>>
 	{
+		#region Properties
+
+		string Name { get; }
+		string this[string value] { get; }
+		IEnumerable<string> Values { get; }
+		IEnumerable<string> ValuesData { get; }
+		int Count { get; }
+		#endregion
+
+		#region Methods
+
+		bool ContainsValue(string value);
+		bool TryGetValue(string value, out string valueData);
+		#endregion
 	}
 }
